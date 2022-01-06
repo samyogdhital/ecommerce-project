4dacec76bcd8982b66ca2289ce5559f3258accec
Samyog Dhital <dhitalsamyog123@gmail.com>
2022-01-07 00:08:04 +0545

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,59 @@
+import { Search } from '@material-ui/icons'
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+height : 60px;
+`
+const Wrapper = styled.div`
+padding: 10px 20px;
+display: flex;
+align-items: center;
+justify-content: space-between;
+`
+const Left = styled.div`
+flex: 1;
+display: flex;
+align-items: center;
+`
+
+const Language = styled.span`
+font-size: 14px;
+cursor: pointer;
+`
+const SearchContainer = styled.div`
+border: 1px solid lightgray;
+display: flex;
+align-items: center;`
+
+const Center = styled.div`
+flex: 2;
+
+`
+const Right = styled.div`
+flex: 1;
+
+`
+const Navbar = () => {
+    return (
+        <Container>
+            <Wrapper>
+                <Left>
+                    <Language>EN</Language>
+                    <SearchContainer>
+                        input
+                        <Search />
+                    </SearchContainer>
+                </Left>
+                <Center>
+                    center
+                </Center>
+                <Right>
+                    right
+                </Right>
+            </Wrapper>
+        </Container>
+    )
+}
+
+export default Navbar
