028dc78b64e7baab37658e5d15d6db0e322c8a2b
Samyog Dhital <dhitalsamyog123@gmail.com>
2022-01-07 00:28:57 +0545

    nav bar finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
-import { Search } from '@material-ui/icons'
+import { Badge } from '@material-ui/core'
+import { Search, ShoppingCartOutlined } from '@material-ui/icons'
 import React from 'react'
 import styled from 'styled-components'
 
@@ -35,6 +36,7 @@ border: none;
 
 const Center = styled.div`
 flex: 2;
+text-align: center;
 `
 
 const Logo = styled.h1`
@@ -43,8 +45,17 @@ font-weight: bold;
 
 const Right = styled.div`
 flex: 1;
-
+display: flex;
+align-items: center;
+justify-content: flex-end;
 `
+
+const MenuItem = styled.div`
+font-size: 14px;
+cursor: pointer;
+margin-left: 25px;
+`
+
 const Navbar = () => {
     return (
         <Container>
@@ -53,14 +64,19 @@ const Navbar = () => {
                     <Language>EN</Language>
                     <SearchContainer>
                         <Input />
-                        <Search />
+                        <Search style={{ color: "gray", fontSize: 16 }} />
                     </SearchContainer>
                 </Left>
                 <Center>
-                    <Logo>Samyog.</Logo>
+                    <Logo>SAMYOG.</Logo>
                 </Center>
                 <Right>
-                    right
+                    <MenuItem>REGISTER</MenuItem>
+                    <MenuItem>SIGN IN</MenuItem>
+                    <MenuItem>SIGN UP</MenuItem>
+                    <Badge badgeContent={4} color="primary">
+                        <ShoppingCartOutlined />
+                    </Badge>
                 </Right>
             </Wrapper>
         </Container>
