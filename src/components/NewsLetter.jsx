3cf6a6d0b215c66881629f983261b552c1a5e020
Samyog Dhital <dhitalsamyog123@gmail.com>
2022-01-08 18:01:09 +0545

    working on newsletter

---
+++ src/components/NewsLetter.jsx
@@ -0,0 +1,46 @@
+import { Send } from "@material-ui/icons"
+import styled from "styled-components"
+
+const Container = styled.div`
+height : 60vh;
+background-color : #fcf5f5;
+display : flex;
+align-item : center;
+justify-content : center;
+flex-direction : column;
+`
+const Title = styled.h1`
+font-size : 70px;
+margin-bottom : 20px;
+`
+const Desc = styled.div`
+font-size : 24px;
+font-weight : 300;
+margin-bottom : 20px;
+`
+const InputContainer = styled.div`
+width : 50%;
+height : 40px;
+background-color : white;
+`
+const Input = styled.input``
+const Button = styled.button``
+
+
+const NewsLetter = () => {
+    return (
+        <Container>
+            <Title>Newsletter</Title>
+            <Desc>Get timely updates from your favorite products.</Desc>
+            <InputContainer>
+                <Input placeholder="Your email" />
+                <Button>
+                    <Send />
+                </Button>
+            </InputContainer>
+
+        </Container>
+    )
+}
+
+export default NewsLetter
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import styled from 'styled-components'
 import { popularProducts } from '../data'
+import Product from './Product'
 
 const Container = styled.div`
 padding : 20px;
 display : flex;
+flex-wrap : wrap;
+justify-content : space-between;
 `
-const Product = styled.div`
 
-`
 
 
 const Products = () => {
