11bf9d2e0b711a873b239e515f2d924820ee7d59
Samyog Dhital <dhitalsamyog123@gmail.com>
2022-01-09 08:45:26 +0545

    making seperate page for each product

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Home from "./pages/Home";
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
 const App = () => {
-  return <div><Home/></div>;
+  return <div><Product /></div>;
 };
 
 export default App;
