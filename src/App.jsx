22862f874ccf10080bccbf46fb42ff6974607772
Samyog Dhital <dhitalsamyog123@gmail.com>
2022-01-09 11:40:01 +0545

    app.js is changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Product from './pages/Product';
-import Cart from "./pages/Cart";
 import Home from "./pages/Home";
-import Login from "./pages/Login";
-import ProductList from "./pages/ProductList";
-import Register from "./pages/Register";
+// import Product from './pages/Product';
+// import Cart from "./pages/Cart";
+// import Login from "./pages/Login";
+// import ProductList from "./pages/ProductList";
+// import Register from "./pages/Register";
 
 const App = () => {
   return <div><Home /></div>;
