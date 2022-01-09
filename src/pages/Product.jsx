f870c8cfc828baf29925e0a48d58d3973408f3d4
Samyog Dhital <dhitalsamyog123@gmail.com>
2022-01-09 10:11:26 +0545

    some changes are made like cart is made and other pages are also made.

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -34,59 +34,64 @@ const Price = styled.span`
 `;
 
 const FilterContainer = styled.div`
-display: flex;
-width: 50%;
-margin: 30px 0;
-justify-content: space-between;
-`
+  display: flex;
+  width: 50%;
+  margin: 30px 0;
+  justify-content: space-between;
+`;
 const Filter = styled.div`
-display: flex;
-align-items: center;
-`
+  display: flex;
+  align-items: center;
+`;
 const FilterTitle = styled.span`
-font-size: 20px;
-font-weight: 200;
-`
+  font-size: 20px;
+  font-weight: 200;
+`;
 const FilterColor = styled.div`
-width: 20px;
-height: 20px;
-border-radius: 50%;
-background-color: ${props => props.color};
-margin: 0 5px;
-cursor: pointer;
-`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+  margin: 0 5px;
+  cursor: pointer;
+`;
 const FilterSize = styled.select`
-margin-left: 10px;
-padding: 5px;
-`
-const FilterSizeOption = styled.option``
+  margin-left: 10px;
+  padding: 5px;
+`;
+const FilterSizeOption = styled.option``;
 const AddContainer = styled.div`
-width: 50%;
-display: flex;
-align-items: center;
-justify-content: space-between;
-`
+  width: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
 const AmountContainer = styled.div`
-display: flex;
-align-items: center;
-font-weight: 700;
-`
+  display: flex;
+  align-items: center;
+  font-weight: 700;
+`;
 const Amount = styled.span`
-width: 30px;
-height: 30px;
-border-radius: 10px;
-border: 1px solid teal;
-display: flex;
-align-items: center;
-justify-content: center;
-margin : 0 5px;
-`
+  width: 30px;
+  height: 30px;
+  border-radius: 10px;
+  border: 1px solid teal;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0 5px;
+`;
 const Button = styled.button`
-padding: 15px;
-border : 1px solid teal;
-background-color: white;
-cursor: pointer;
-`
+  padding: 15px;
+  border: 2px solid teal;
+  background-color: white;
+  cursor: pointer;
+  font-weight: 500;
+
+  &:hover {
+    background-color: #f8f4f4;
+  }
+`;
 
 const Product = () => {
     return (
@@ -122,7 +127,6 @@ const Product = () => {
                                 <FilterSizeOption>M</FilterSizeOption>
                                 <FilterSizeOption>L</FilterSizeOption>
                                 <FilterSizeOption>XL</FilterSizeOption>
-
                             </FilterSize>
                         </Filter>
                     </FilterContainer>
